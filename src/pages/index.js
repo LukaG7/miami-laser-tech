5e975cf0d0ee5503e6ccb40f64afbe9b5f3e5b47
Lukas Gonzalez <lukegonz47@gmail.com>
2023-03-02 14:28:40 -0500

    readded it into index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,7 @@ const Home = () => {
       <AppAppBar />
       <ProductHero />
       <ProductValues />
-      {/* <ProductCategories /> */}
+      <ProductCategories />
       <ProductHowItWorks />
       <ProductCTA />
       <ProductSmokingHero />
